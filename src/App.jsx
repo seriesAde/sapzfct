76bdb9933a18bdfd427001e32571df8845722efb
Adeshina Ahmed <whygee007@gmail.com>
2025-10-26 04:33:23 +0100

    fixed home

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Navbar/>
       <main>
         <Routes>
-          <Route path="./" element={<Home />} />
+          <Route path="/" element={<Home />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
       </main>
